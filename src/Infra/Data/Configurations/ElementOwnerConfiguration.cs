31b22d785bbddcd9e7cfb3b3828b7a7ab69795af
Nagasudhir <Nagasudhir@SUDHIR>
2024-11-01 16:22:43 +0530

    many to many unique key

---
+++ src/Infra/Data/Configurations/ElementOwnerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infra.Data.Configurations;
+
+public class ElementOwnerConfiguration : IEntityTypeConfiguration<ElementOwner>
+{
+    public void Configure(EntityTypeBuilder<ElementOwner> builder)
+    {
+        builder.HasKey(e => new { e.ElementId, e.OwnerId });
+    }
+}
---
+++ src/Infra/Data/Configurations/SubstationOwnerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infra.Data.Configurations;
+
+public class SubstationOwnerConfiguration : IEntityTypeConfiguration<SubstationOwner>
+{
+    public void Configure(EntityTypeBuilder<SubstationOwner> builder)
+    {
+        builder.HasKey(e => new { e.SubstationId, e.OwnerId });
+    }
+}
