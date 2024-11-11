747975f9547962fad5cf48198794b1a07557ca97
Nagasudhir <Nagasudhir@SUDHIR>
2024-11-11 13:37:48 +0530

    create line handler done

--- src/App/Lines/Commands/CreateLine/CreateLine.cs
+++ src/App/Lines/Commands/CreateLine/CreateLine.cs
@@ -1,5 +1,11 @@
 ﻿using App.Common.Behaviours;
+using App.Common.Interfaces;
+using App.Owners.Utils;
+using Core.Entities.Elements;
+using Core.Entities;
+using FluentValidation.Results;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.Lines.Commands.CreateLine;
 
@@ -17,4 +23,58 @@ public record CreateLineCommand : IRequest<int>
     public double Length { get; set; }
     public required string ConductorType { get; set; }
     public bool IsAutoReclosurePresent { get; set; }
+}
+
+public class CreateLineCommandHandler(IApplicationDbContext context) : IRequestHandler<CreateLineCommand, int>
+{
+    public async Task<int> Handle(CreateLineCommand request, CancellationToken cancellationToken)
+    {
+        Bus bus1 = await context.Buses
+                .AsNoTracking()
+                .FirstOrDefaultAsync(e => e.Id == request.Bus1Id, cancellationToken: cancellationToken)
+                ?? throw new Common.Exceptions.ValidationException([new ValidationFailure() {
+                                                                                    ErrorMessage = "Bus Id is not present in database"
+                                                                                }]);
+        Bus bus2 = await context.Buses
+                .AsNoTracking()
+                .FirstOrDefaultAsync(e => e.Id == request.Bus2Id, cancellationToken: cancellationToken)
+                ?? throw new Common.Exceptions.ValidationException([new ValidationFailure() {
+                                                                                    ErrorMessage = "Bus Id is not present in database"
+                                                                                }]);
+        (string lineName, string voltLevel, string region) = await Utils.DeriveLineName.ExecuteAsync(request.Bus1Id, request.Bus2Id, request.ElementNumber, context, cancellationToken);
+
+        // derive owner names cache
+        List<Owner> owners = await OwnerUtils.GetOwnersFromIdsAsync(request.OwnerIds, context, cancellationToken);
+        string ownersNames = OwnerUtils.DeriveOwnersCache(owners);
+
+        // insert bus to db
+        var entity = new Line()
+        {
+            ElementNameCache = lineName,
+            VoltLevelCache = voltLevel,
+            RegionCache = region,
+            Substation1Id = bus1.Substation1Id,
+            Substation2Id = bus2.Substation1Id,
+            OwnerNamesCache = ownersNames,
+            ElementNumber = request.ElementNumber,
+            CommissioningDate = request.CommissioningDate,
+            DeCommissioningDate = request.DeCommissioningDate,
+            CommercialOperationDate = request.CommercialOperationDate,
+            IsImportantGridElement = request.IsImportantGridElement,
+            Bus1Id = request.Bus1Id,
+            Bus2Id = request.Bus2Id,
+            Length = request.Length,
+            ConductorType = request.ConductorType,
+            IsAutoReclosurePresent = request.IsAutoReclosurePresent
+        };
+
+        context.Lines.Add(entity);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        // create ElementOwners entries
+        await OwnerUtils.InsertElementOwnersAsync(entity.Id, owners.Select(o => o.Id), context, cancellationToken);
+
+        return entity.Id;
+    }
 }
---
+++ src/App/Lines/Utils/DeriveLineName.cs
@@ -0,0 +1,32 @@
+﻿using App.Common.Interfaces;
+using Core.Entities.Elements;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace App.Lines.Utils;
+
+public static class DeriveLineName
+{
+    public static async Task<(string lineName, string voltLevel, string region)> ExecuteAsync(int bus1Id, int bus2Id, string elementNumber, IApplicationDbContext context, CancellationToken cancellationToken)
+    {
+        Bus bus1 = await context.Buses
+                .AsNoTracking()
+                .Include(e => e.Substation1)
+                .ThenInclude(s => s.Location).FirstOrDefaultAsync(e => e.Id == bus1Id, cancellationToken: cancellationToken) ?? throw new Common.Exceptions.ValidationException([new ValidationFailure() {
+                                                                                    ErrorMessage = "Substation Id is not present in database"
+                                                                                }]);
+        Bus bus2 = await context.Buses
+                .AsNoTracking()
+                .Include(e => e.Substation1)
+                .ThenInclude(s => s.Location).FirstOrDefaultAsync(e => e.Id == bus2Id, cancellationToken: cancellationToken) ?? throw new Common.Exceptions.ValidationException([new ValidationFailure() {
+                                                                                    ErrorMessage = "Substation Id is not present in database"
+                                                                                }]);
+        string voltLvl = bus1.VoltLevelCache;
+        string location1 = bus1.Substation1.Location.Name;
+        string location2 = bus2.Substation1.Location.Name;
+        string lineName = $"{voltLvl}-{location1}-{location2}-{elementNumber}";
+        string region = $"{bus1.RegionCache}-{bus2.RegionCache}";
+        return (lineName, voltLvl, region);
+    }
+}
