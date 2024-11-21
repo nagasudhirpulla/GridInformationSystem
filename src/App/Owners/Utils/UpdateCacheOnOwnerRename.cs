0c34f8dee54b503e72226469da9a27a3feb1bb78
Nagasudhir <Nagasudhir@SUDHIR>
2024-11-21 18:20:38 +0530

    owner cache update in progess

---
+++ src/App/Owners/Utils/UpdateCacheOnOwnerRename.cs
@@ -0,0 +1,25 @@
+﻿using App.Common.Interfaces;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Owners.Utils;
+
+public class UpdateCacheOnOwnerRename
+{
+    public static async Task ExecuteAsync(string existingOwnerName, string newOwnerName, IApplicationDbContext context, CancellationToken cancellationToken)
+    {
+        // find all entities which have the property "OwnerNamesCache"
+        var entityTypes = context.GetType()
+                            .GetProperties()
+                            .Where(prop => prop.PropertyType.GetProperties()
+                                .Any(p => p.Name.Equals("OwnerNamesCache", StringComparison.OrdinalIgnoreCase)))
+                            .Select(prop => prop.PropertyType)
+                            .ToList() ?? [];
+
+        // Search for "existingOwnerName;" and replace with "newOwnerName;" in all the entitites
+    }
+}
