71f4f4a92e7f4c860c9aecb8f58eadff5f1d21c2
Nagasudhir <Nagasudhir@SUDHIR>
2025-03-04 15:35:01 +0530

    started transformer create app layer

---
+++ src/App/Transformers/Commands/CreateTransformer.cs
@@ -0,0 +1,20 @@
+﻿using App.Common.Behaviours;
+using Core.Enums;
+using MediatR;
+
+namespace App.Transformers.Commands;
+
+[Transactional(IsolationLevel = System.Data.IsolationLevel.Serializable)]
+public record CreateTransformerCommand : IRequest<int>
+{
+    public int Substation1Id { get; set; }
+    public int Substation2Id { get; set; }
+    public required string OwnerIds { get; set; }
+    public required string ElementNumber { get; set; }
+    public DateTime CommissioningDate { get; set; }
+    public DateTime? DeCommissioningDate { get; set; }
+    public DateTime CommercialOperationDate { get; set; }
+    public bool IsImportantGridElement { get; set; } = false;
+    public TransformerTypeEnum TransformerType { get; set; } = null!;
+    public double MvaCapacity { get; set; }
+}
