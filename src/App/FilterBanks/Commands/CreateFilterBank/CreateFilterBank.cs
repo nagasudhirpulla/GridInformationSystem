3120b4adc795674307fe85855677754f586f256c
Nagasudhir <Nagasudhir@SUDHIR>
2025-04-07 18:13:41 +0530

    started filter bank app layer create command

---
+++ src/App/FilterBanks/Commands/CreateFilterBank/CreateFilterBank.cs
@@ -0,0 +1,18 @@
+﻿using App.Common.Behaviours;
+using MediatR;
+
+namespace App.FilterBanks.Commands.CreateFilterBank;
+
+[Transactional(IsolationLevel = System.Data.IsolationLevel.Serializable)]
+public record CreateFilterBankCommand : IRequest<int>
+{
+    public int SubstationId { get; set; }
+    public required string OwnerIds { get; set; }
+    public required string ElementNumber { get; set; }
+    public DateTime CommissioningDate { get; set; }
+    public DateTime? DeCommissioningDate { get; set; }
+    public DateTime CommercialOperationDate { get; set; }
+    public bool IsImportantGridElement { get; set; } = false;
+    public double Mvar { get; set; }
+    public bool IsSwitchable { get; set; }
+}
